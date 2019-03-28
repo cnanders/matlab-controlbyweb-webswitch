ae7327b5f7e3782bf6358695aea7c2ba0c5c7997
Chris Anderson <cnanders@gmail.com>
2019-03-28 11:57:47 -0700

    namespacing abstract class that is emplemented

--- src/+controlbyweb/WebSwitch.m
+++ src/+controlbyweb/WebSwitch.m
@@ -1,4 +1,4 @@
-classdef WebSwitch < AbstractWebSwitch
+classdef WebSwitch < controlbyweb.AbstractWebSwitch
     %UNTITLED Summary of this class goes here
     %   Detailed explanation goes here
     
--- src/+controlbyweb/WebSwitchVirtual.m
+++ src/+controlbyweb/WebSwitchVirtual.m
@@ -1,4 +1,4 @@
-classdef WebSwitchVirtual < AbstractWebSwitch
+classdef WebSwitchVirtual < controlbyweb.AbstractWebSwitch
     %UNTITLED Summary of this class goes here
     %   Detailed explanation goes here
     
